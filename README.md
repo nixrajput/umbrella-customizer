0e46d46ecb55034ca48b179beba19000c3273277
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2024-06-24 00:42:26 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# umbrella-customizer
+
+You have to create an umbrella customizer for an umbrella seller. The seller offers these umbrellas in multiple colors and the corporate brands are their main customers. These brands want the seller to print their logo on the umbrella. The seller wants you to create a tool that'll help their customers create an instant preview of the customized umbrella on the website by adding the logo.
+
+The customizer tool will let the brand upload its logo and will create a preview by adding the logo to the bottom of the umbrella. The umbrella is available in multiple colors and a user can change the color using color swatch buttons. On selecting the color of the umbrella, the theme of the whole customization tool changes, and the umbrella in new color loads. You can find all these details of the customizer tool in the demo photos and video below. You have to recreate the design and functionality as shown in the demo.
+
+In the demo image, there are 8 colors, however, you just need to create this interface for 3 colors Pink, Blue, and Yellow.
---
+++ scripts/main.js
@@ -0,0 +1,108 @@
+"use strict";
+
+{
+  // Get the references to the DOM elements
+  const colorButtons = document.querySelectorAll(".color-swatch");
+  const umbrellaImage = document.getElementById("umbrella-image");
+  const uploadButton = document.getElementById("upload");
+  const fileUploader = document.getElementById("fileUploader");
+  const logoField = document.getElementById("logo");
+  const loadingIcon = document.getElementById("loading");
+
+  let isLogoSelected = false;
+  let seletedButton = "blue";
+
+  function init() {
+    window.addEventListener("load", () => {
+      colorButtons.forEach((button) => {
+        if (button.dataset.color === seletedButton) {
+          button.classList.add(`${button.dataset.color}-swatch-border`);
+        }
+      });
+    });
+  }
+
+  // Add event listener for each color button
+  colorButtons.forEach((button) => {
+    button.addEventListener("click", async () => {
+      loadingIcon.classList.remove(button.dataset.filterClass);
+      loadingIcon.style.filter = button.dataset.filter;
+      const colorName = button.dataset.color;
+      const color = button.style.backgroundColor;
+      seletedButton = colorName;
+      removeButtonBorder();
+      button.classList.add(`${colorName}-swatch-border`);
+      changeTheme(colorName, color);
+      displayLoadingIcon();
+      await delay(2000);
+      removeLoadingIcon();
+    });
+  });
+
+  function removeButtonBorder() {
+    colorButtons.forEach((button) => {
+      if (button.dataset.color !== seletedButton) {
+        button.classList.remove(`${button.dataset.color}-swatch-border`);
+      }
+    });
+  }
+
+  // changeTheme is used ro change the umbrella image and colors
+  // whenever one of the color buttons are clicked
+  function changeTheme(colorName, color) {
+    umbrellaImage.src = `assets/${colorName}_umbrella.png`;
+    uploadButton.style.backgroundColor = color;
+  }
+
+  // The fileuploader is actually an hidden element and is clicked
+  // whenever uploadbutton is clicked.
+  uploadButton.onclick = () => {
+    fileUploader.click();
+  };
+
+  function displayLoadingIcon() {
+    loadingIcon.classList.remove("disabled");
+    umbrellaImage.classList.add("disabled");
+    logoField.classList.add("disabled");
+  }
+
+  function removeLoadingIcon() {
+    loadingIcon.classList.add("disabled");
+    umbrellaImage.classList.remove("disabled");
+    if (isLogoSelected) {
+      logoField.classList.remove("disabled");
+    }
+  }
+
+  function removeLogoIcon() {
+    logoField.classList.add("disabled");
+  }
+
+  function displayLogoIcon() {
+    logoField.classList.remove("disabled");
+  }
+
+  // whenever a file is uploaded this function is called.
+  fileUploader.onchange = function () {
+    if (this.files && this.files[0]) {
+      const reader = new FileReader();
+      reader.onload = loadImage;
+      reader.readAsDataURL(this.files[0]);
+      removeLogoIcon();
+    }
+  };
+
+  async function loadImage(e) {
+    displayLoadingIcon();
+    await delay(2000);
+    removeLoadingIcon();
+    displayLogoIcon();
+    logoField.src = e.target.result;
+    isLogoSelected = true;
+  }
+
+  // Use to delay program execution asynchronously
+  const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+  init();
+}
